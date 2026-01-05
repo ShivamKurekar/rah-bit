8c9387bd87908eb52bb890ffad638419b4f556a9
ShivamKurekar <kurekarshivam@gmail.com>
2026-01-05 19:11:18 +0530

    feat(adder.v): Input issue
    Earlier module was supposed to take two inputs, but even when only one input is given it takes it for 1st argument and takes it for second argument too
    
    Added 2nd input compulsion, without second input it doesnt give any output, i.e. two inputs are compulsary
    
    Signed-off-by: ShivamKurekar <kurekarshivam@gmail.com>

--- examples/calc_ops/rtl/adder.v
+++ examples/calc_ops/rtl/adder.v
@@ -11,8 +11,8 @@ module adder (
 parameter RAH_PACKET_WIDTH = 48;
 
 localparam IDLE = 2'd0;
-localparam NEXT = 2'd1;
-localparam LB = 2'd2;
+localparam LOAD_a = 2'd1;
+localparam LOAD_b = 2'd2;
 localparam ADD = 2'd3;
 
 reg [RAH_PACKET_WIDTH-1:0] da = 0;
@@ -25,28 +25,36 @@ always @(posedge clk) begin
         IDLE:begin
             wren <= 0;
 
-            if (~empty) begin
+            if (!empty) begin
                rden <= 1;
-               state <= NEXT;
+               state <= LOAD_a;
             end else begin
                rden <= 0;
             end
         end
 
-        NEXT: begin
+        LOAD_a: begin
             if (r_wait) begin
-                da <= a;
-                state <= LB;
-                r_wait <= 0;
+                if (!empty) begin
+                    da <= a;
+                    state <= LOAD_b;
+                    r_wait <= 0;
+                end
+                else begin
+                end
             end else begin
                 r_wait <= ~r_wait;
             end
         end
 
-        LB: begin
+        LOAD_b: begin
+            if(!empty) begin
             db <= a;
             rden <= 0;
             state <= ADD;
+            end
+            else
+            state <= LOAD_b;
         end
 
         ADD: begin
